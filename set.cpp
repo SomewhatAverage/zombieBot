88c16dd1fc97681aaa1b03d30f511835d5a7f87f
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-09 18:35:00 -0400

    Create set.cpp

---
+++ set.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argv[0] == "perk") {
+  
+  } else if (argv[0] == "weapon") {
+    
+  }
+
+}
