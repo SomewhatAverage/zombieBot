969c431ecc3d6645751031a5a5789d0492aa5698
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-12 16:30:42 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,25 +1,7 @@
 var Help = require("./commands/help.js");
+var Chest = require("./commands/chest.js");
 
 module.exports = {
   var help = Help.help;
-  
-  var chest = (
-    "Everything about the weapons chest :" +
-    "- What is it? The weapons chest is a chest that you can roll to get new weapons." +
-    "- Price? Each roll costs 1000 gold." +
-    "- Does it TP? After few rolls the weapons chest will be "teleported" to an other room which has a chest in it. It is normal." +
-    "- How can I know in which room the chest is?" +
-    "> You can see the \"Weapon chest\" through walls (it's an armor_stand name) and guess in which room it is." +
-    "> Right click on the chest will tell you in which room it is currently located." +
-    "> Outside you have a purple beacon which indicated the place of the chest." +
-    "- Is there a limit ? No, you can roll as many times as you want." +
-    "- Which weapons can I obtain from it ?" +
-    ":flamethrower: <w flamethrower for details" +
-    ":zombie_soaker: <w zombie_soaker for details" +
-    ":blow_dart: <w blow_dart for details" +
-    ":gold_digger: <w gold_digger for details" +
-    ":zombie_zapper: <w zombie_zapper for details" +
-    ":elder_gun: <w elder_gun for details" +
-    ":puncher: <w puncher for details"
-  ); */
+  var chest = Chest.chest;
 }
