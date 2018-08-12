0f17a383b505fe52e023b1cf426045bc23ef7a5d
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-12 16:42:02 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,30 +1,29 @@
-module.exports {
-  var help = (
-    "Zombies [BOT] created by [Owner] Percy84 and copied by [Copier] TitaniumTahmid\n" +
-    "Credit to LittlePhilip and Skyline294 for informations about weapons, perks and rooms!\n" +
-    ">>> Current commands:\n" +
-    "- <chest -> everything about the weapons chest" +
-    "- <ez -> displays a random message among hypixel replacement messages (only command which does not need to be used in botchat)\n" +
-    "- <help -> displays this message\n" +
-    "- <machine -> everything about team machine\n" +
-    "- <p name_perk -> informations about the written perk\n" +
-    "- <perks -> everything about perks + list\n" +
-    "- <r name_room -> informations about the written room\n" +
-    "- <rooms -> everything about rooms + list\n" +
-    "- <switch -> everything about power switch\n" +
-    "- <w name_weapon -> informations about the written weapon\n" +
-    "- <weapons -> everything about weapons + list\n" +
-    "- <zombies -> displays the story of the Zombies minigame\n" +
-    "\n" +
-    " >>> You can create a personal set for Zombies!\n" +
-    "- <set display -> displays your set\n" +
-    "- <set name name of you set (you can use spaces)\n" +
-    "- <set perks perk_1 perk_2 perk_3 - '<perks' for the perks list\n" +
-    "- <set weapons weapon_1 weapon_2 weapon_3 (if extra_weapon in your perks) - '<weapons' for the weapons list\n" +
-    "\n" +
-    ">>> Others :\n" +
-    "- <version -> displays the version of the bot\n" +
-    "- <report bug describe bug here -> report command bugs like a command which does not work, sets not working, etc\n" +
-    "- <report text describe the problem here -> report text errors like a misspelling or an emoji which does not work, etc\n"
-  )
-}
+exports.helpCMD = (
+  "Zombies [BOT] created by [Owner] Percy84 and copied by [Copier] TitaniumTahmid\n" +
+  "Credit to LittlePhilip and Skyline294 for informations about weapons, perks and rooms!\n" +
+  ">>> Current commands:\n" +
+  "- <chest -> everything about the weapons chest" +
+  "- <ez -> displays a random message among hypixel replacement messages (only command which does not need to be used in botchat)\n" +
+  "- <help -> displays this message\n" +
+  "- <machine -> everything about team machine\n" +
+  "- <p name_perk -> informations about the written perk\n" +
+  "- <perks -> everything about perks + list\n" +
+  "- <r name_room -> informations about the written room\n" +
+  "- <rooms -> everything about rooms + list\n" +
+  "- <switch -> everything about power switch\n" +
+  "- <w name_weapon -> informations about the written weapon\n" +
+  "- <weapons -> everything about weapons + list\n" +
+  "- <zombies -> displays the story of the Zombies minigame\n" +
+  "\n" +
+  " >>> You can create a personal set for Zombies!\n" +
+  "- <set display -> displays your set\n" +
+  "- <set name name of you set (you can use spaces)\n" +
+  "- <set perks perk_1 perk_2 perk_3 - '<perks' for the perks list\n" +
+  "- <set weapons weapon_1 weapon_2 weapon_3 (if extra_weapon in your perks) - '<weapons' for the weapons list\n" +
+  "\n" +
+  ">>> Others :\n" +
+  "- <version -> displays the version of the bot\n" +
+  "- <report bug describe bug here -> report command bugs like a command which does not work, sets not working, etc\n" +
+  "- <report text describe the problem here -> report text errors like a misspelling or an emoji which does not work, etc\n"
+);
+
