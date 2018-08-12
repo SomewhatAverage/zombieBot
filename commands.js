90f5856e28c4e3acabd8c83bba3704dddf5c895a
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-12 16:27:35 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,32 +1,7 @@
+var Help = require("./commands/help.js");
+
 module.exports = {
-  /* var help = (
-    "Zombies [BOT] created by [Owner] Percy84 and copied by [Copier] TitaniumTahmid" +
-    "Credit to LittlePhilip and Skyline294 for informations about weapons, perks and rooms!" +
-    ">>> Current commands:" +
-    "- <chest -> everything about the weapons chest" +
-    "- <ez -> displays a random message among hypixel replacement messages (only command which does not need to be used in botchat)" +
-    "- <help -> displays this message" +
-    "- <machine -> everything about team machine" +
-    "- <p name_perk -> informations about the written perk" +
-    "- <perks -> everything about perks + list" +
-    "- <r name_room -> informations about the written room" +
-    "- <rooms -> everything about rooms + list" +
-    "- <switch -> everything about power switch" +
-    "- <w name_weapon -> informations about the written weapon" +
-    "- <weapons -> everything about weapons + list" +
-    "- <zombies -> displays the story of the Zombies minigame" +
-    "\n" +
-    " >>> You can create a personal set for Zombies!" +
-    "- <set display -> displays your set" +
-    "- <set name name of you set (you can use spaces)" +
-    "- <set perks perk_1 perk_2 perk_3 - '<perks' for the perks list" +
-    "- <set weapons weapon_1 weapon_2 weapon_3 (if extra_weapon in your perks) - '<weapons' for the weapons list" +
-    "\n" +
-    ">>> Others :" +
-    "- <version -> displays the version of the bot" +
-    "- <report bug describe bug here -> report command bugs like a command which does not work, sets not working, etc" +
-    "- <report text describe the problem here -> report text errors like a misspelling or an emoji which does not work, etc"
-  );
+  var help = Help.help;
   
   var chest = (
     "Everything about the weapons chest :" +
