e2601adce5d2214dfd6cb1dd1f4da4e657af0cda
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-12 16:48:12 -0400

    Create machine.js

---
+++ commands/machine.js
@@ -0,0 +1,10 @@
+exports.machineCMD = (
+  "Everything about the team machine:\n" +
+  "- Where is it?\n" +
+  "    > In Dead End: Roof Top\n" +
+  "    > In Bad Blood: Great Hall\n" +
+  "- What is its utility? With the team machine you can buy 3 things (at will):\n" +
+  "    > Ammo Supply (arrow) -> refill the ammos all the players.\n" +
+  "    > Full Revive (golden apple) -> Revive all teammates died or knocked down.\n" +
+  "    > Dragon Warth (dragon egg) -> Kill all mobs in a radius of 15 around the team machine, the price increases of 1,000 each time you use it.\n"
+)
