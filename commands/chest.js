a6fd6a7c0fbe4d4f019557a36154ce8a14cf2c95
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-12 16:29:47 -0400

    Update chest.js

--- commands/chest.js
+++ commands/chest.js
@@ -1,19 +1,21 @@
-var chest = (
-   "Everything about the weapons chest :\n" +
-   "- What is it? The weapons chest is a chest that you can roll to get new weapons.\n" +
-   "- Price? Each roll costs 1000 gold.\n" +
-   "- Does it TP? After few rolls the weapons chest will be \"teleported\" to an other room which has a chest in it. It is normal.\n" +
-   "- How can I know in which room the chest is?\n" +
-   "> You can see the \"Weapon chest\" through walls (it's an armor_stand name) and guess in which room it is.\n" +
-   "> Right click on the chest will tell you in which room it is currently located.\n" +
-   "> Outside you have a purple beacon which indicated the place of the chest.\n" +
-   "- Is there a limit ? No, you can roll as many times as you want.\n" +
-   "- Which weapons can I obtain from it ?\n" +
-   "<:flamethrower:478295712887472132> <w flamethrower for details\n" +
-   "<:zombie_soaker:478295712745127987> <w zombie_soaker for details\n" +
-   "<:blow_dart:478295713252507671> <w blow_dart for details\n" +
-   "<:gold_digger:478295712971489300> <w gold_digger for details\n" +
-   "<:zombie_zapper:478295712958906377> <w zombie_zapper for details\n" +
-   "<:elder_gun:478295713034403853> <w elder_gun for details\n" +
-   "<:puncher:478295713055506434> <w puncher for details\n"
+module.exports {
+  var help = (
+    "Everything about the weapons chest :\n" +
+    "- What is it? The weapons chest is a chest that you can roll to get new weapons.\n" +
+    "- Price? Each roll costs 1000 gold.\n" +
+    "- Does it TP? After few rolls the weapons chest will be \"teleported\" to an other room which has a chest in it. It is normal.\n" +
+    "- How can I know in which room the chest is?\n" +
+    "> You can see the \"Weapon chest\" through walls (it's an armor_stand name) and guess in which room it is.\n" +
+    "> Right click on the chest will tell you in which room it is currently located.\n" +
+    "> Outside you have a purple beacon which indicated the place of the chest.\n" +
+    "- Is there a limit ? No, you can roll as many times as you want.\n" +
+    "- Which weapons can I obtain from it ?\n" +
+    "<:flamethrower:478295712887472132> <w flamethrower for details\n" +
+    "<:zombie_soaker:478295712745127987> <w zombie_soaker for details\n" +
+    "<:blow_dart:478295713252507671> <w blow_dart for details\n" +
+    "<:gold_digger:478295712971489300> <w gold_digger for details\n" +
+    "<:zombie_zapper:478295712958906377> <w zombie_zapper for details\n" +
+    "<:elder_gun:478295713034403853> <w elder_gun for details\n" +
+    "<:puncher:478295713055506434> <w puncher for details\n"
   );
+}
