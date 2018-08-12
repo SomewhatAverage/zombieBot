e9cecf6d7a5b07fec2e4b57d8af0e8c0394dc73b
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-12 16:48:59 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,5 +1,7 @@
 var HelpOBJ = require("./commands/help.js");
 var ChestOBJ = require("./commands/chest.js");
+var machineOBJ = require("./commands/machine.js");
 
 exports.help = HelpOBJ.helpCMD;
 exports.chest = ChestOBJ.chestCMD;
+exports.machine = machineOBJ.machineCMD;
