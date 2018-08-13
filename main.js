a9adcb45995fd2ca2be2c74a768bba969edb2cad
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-13 19:02:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ const bot = new Discord.Client();
 const stringCommands = require("./stringCommands.js");
 const argCommands = require("./argCommands.js");
 
+const fs = require("fs");
+
 bot.on('ready', () => {
   console.log('Hello World!');
 });
 
 const prefix = "<";
+
+const reportPath = "./data/lastReport.txt";
 
 bot.on('message', message => {
   if (message.author.bot) return;
@@ -19,10 +23,28 @@ bot.on('message', message => {
   if (stringCommands[command]) {
     message.channel.send(stringCommands[command]);
   } else if (argCommands[command]) {
-    message.channel.send(argCommands[command](args[0]));
+    message.channel.send(argCommands[command](message, args));
   } else {
 		const fluctuatingCommand = require("./commands/" + command + ".js");
 		message.channel.send(fluctuatingCommand.mainCMD());
   }
-});
   
+  fs.readFile(reportPath, "utf8", function (err, data) {
+    if (err) throw (err);
+    
+    if (data != "") {
+      const owner = bot.fetchUser(process.env.OWNER_ID)
+        .then((User) => User.send(data))
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    
+    fs.writeFile(reportPath, "", function (err) {
+      if (err) throw (err);
+    });
+    
+  });
+  
+});
+bot.login(process.env.BOT_TOKEN);
