4077884ed2fcb9c9875f8dc6b14d82db331892cf
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-12 22:59:12 -0400

    Update perkList.js

--- commands/perks/perkList.js
+++ commands/perks/perkList.js
@@ -1,1 +1,71 @@
+var extra_health = {
+  emoji : "<:extra_health:478382875842969611>",
+  description : "**",
+  price : 1000,
+  location : {
+    deadEnd : "Roof Top",
+    badBlood : "Dungeon"
+  }
+};
+
+var quick_fire = {
+  emoji : "<:quick_fire:478382875910078489>",
+  description : "*Increase the fire rate of all guns by 25%.*",
+  price : 1000,
+  location : {
+    deadEnd : "Apartments",
+    badBlood : "Mansion"
+  }
+};
+
+var fast_revive = {
+  emoji : "<:fast_revive:478382875838906375>",
+  description : "*Revive fallen teammates much faster.*",
+  price : 500,
+  location : {
+    deadEnd : "Alley",
+    badBlood : "Courtyard"
+  }
+};
+
+var extra_weapon = {
+  emoji : "<:extra_weapon:478382875889238026>",
+  description : "*Gain an extra weapon slot.*",
+  price : 1500,
+  location : {
+    deadEnd : "Office",
+    badBlood : "Library"
+  }
+}
+
+var frozen_bullets = {
+  emoji : "<:frozen_bullets:478382875872460802>",
+  description : "*All your bullets are ice cold, slowing enemies down briefly.*",
+  price : 1000,
+  location : {
+    deadEnd : "Hotel",
+    badBlood : "Balcony"
+  }
+}
+
+
+var flame_bullets = {
+  emoji : "<:flame_bullets:478382875465744392>",
+  description : "*All your bullets set enemies on fire, including enemies immune to fire.*",
+  price : 750,
+  location : {
+    deadEnd : "Hotel"
+  }
+}
+
+
+var speed = {
+  emoji : "<:speed:478382876098822148>",
+  description : "*Gain Speed I.*",
+  price : 500,
+  location : {
+    deadEnd : "Gallery",
+    badBlood : "Crypts"
+  }
+}
 
