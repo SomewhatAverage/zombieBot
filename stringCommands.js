4fb65f1ef92a76b629f948c91687cb7a65bc1edf
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-13 15:39:21 -0400

    Update stringCommands.js

--- stringCommands.js
+++ stringCommands.js
@@ -4,6 +4,7 @@ var MachineOBJ = require("./commands/machine.js");
 var SwitchOBJ = require("./commands/switch.js");
 var PerksOBJ = require("./commands/perks.js");
 var RoomsOBJ = require("./commands/rooms.js");
+var WeaponsOBJ = require("./commands/weapons.js");
 var zombiesOBJ = require("./commands/zombies.js");
 
 exports.help = HelpOBJ.helpCMD;
@@ -12,4 +13,5 @@ exports.machine = MachineOBJ.machineCMD;
 exports.switch = SwitchOBJ.switchCMD;
 exports.perks = PerksOBJ.perksCMD;
 exports.rooms = RoomsOBJ.roomsCMD;
+exports.weapons = WeaponsOBJ.weaponsCMD;
 exports.zombies = zombiesOBJ.zombiesCMD;
