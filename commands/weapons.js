6cd8fa8070acedf006556f2fc91ae40706372fc6
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-13 15:38:28 -0400

    Create weapons.js

---
+++ commands/weapons.js
@@ -0,0 +1,28 @@
+exports.weaponsCMD = (
+  ">> Everything about **weapons**:\n" +
+  "- Weapons are there to help you kill zombies. You have a lot of different weapons to pick.\n" +
+  "- You can also ultimate your weapons at the ultimate machine:\n" +
+  "    Cost : **1500** gold\n" +
+  "    Location in Dead End : **Garden**\n" +
+  "    Location in Bad Blood : **Graveyard**\n" +
+  "You can upgrade a weapon only once, except the Gold Digger (you can upgrade it 5 times, being the only weapon that you can upgrade more than 1 time)\n" +
+  "- In the first slot you have your knife. It is your starter melee weapon. You can only replace it with The Puncher.\n" +
+  "- You can have up to 2 guns. With extra weapon perk you can have 1 more slot: <p extra_weapon for details.\n" +
+  "- If you already have all your gun slots filled and if you want to buy a new gun, you need to replace one of those you have by buying your new gun while holding the one you want to replace.\n" +
+  "- Details about a weapon? **<w name_weapon**\n" +
+  "\n" +
+  ">> List of all the weapons in Zombies:\n" +
+  "<:pistol:478295712946323460> **<w pistol** for details\n" +
+  "<:rifle:478295713051181086> **<w rifle** for details\n" +
+  "<:shotgun:478295713285931009> **<w shotgun** for details\n" +
+  "<:flamethrower:478295712887472132> **<w flamethrower** for details\n" +
+  "<:zombie_soaker:478295712745127987> **<w zombie_soaker** for details\n" +
+  "<:sniper:478295713009238016> **<w sniper** for details\n" +
+  "<:rocket_launcher:478295713139130379> **<w rocket_launcher** for details\n" +
+  "<:blow_dart:478295713252507671> **<w blow_dart** for details\n" +
+  "<:gold_digger:478295712971489300> **<w gold_digger** for details\n" +
+  "<:zombie_zapper:478295712958906377> **<w zombie_zapper** for details\n" +
+  "<:elder_gun:478295713034403853> **<w elder_gun** for details\n" +
+  "<:sword:478295712929546240> **<w knife** for details\n" +
+  "<:puncher:478295713055506434> **<w puncher** for details\n"
+);
