d724e183d4821434f240b61bebb112e0b4d3c835
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-08-12 23:16:41 -0400

    Update stringCommands.js

--- stringCommands.js
+++ stringCommands.js
@@ -6,4 +6,4 @@ var ezOBJ = require("./commands/ez.js");
 exports.help = HelpOBJ.helpCMD;
 exports.chest = ChestOBJ.chestCMD;
 exports.machine = machineOBJ.machineCMD;
-exports.ez = ezOBJ.ezCMD;
+exports.ez = new ezOBJ.ezCMD;
